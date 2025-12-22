11c81874cd5285fc4de60e692412735c0847826c
saravanamoorthir <saravanamoorthir@gmail.com>
2025-12-22 21:47:21 +0530

    Initial Flutter project

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/pages/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+@override
+State<HomePage> createState()=> _HomePageState();
+}
+
+
+class _HomePageState extends State<HomePage>{
+@override
+Widget build(BuildContext context){
+return Scaffold(
+  backgroundColor: Colors.yellow[200],
+  appBar: AppBar(
+    title: Text('To Do'),
+    elevation: 0,
+  ),
+body: ListView(
+  children: [
+    TodoTile(),
+  ],
+),
+);
+
+}
+  }
