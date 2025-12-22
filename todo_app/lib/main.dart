2c9da93f64c8fefb13003af93b23197ade643a84
saravanamoorthir <saravanamoorthir@gmail.com>
2025-12-22 22:03:49 +0530

    correcting the todo folder path issue in importing

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/pages/util/todo_tile.dart';
+import 'package:todo_app/pages/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
